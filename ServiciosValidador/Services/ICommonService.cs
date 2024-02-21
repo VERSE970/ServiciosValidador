b5da34dc757f18bf488ca30b72e14d836ee0af97
VERSE970 <braumblitz69@gmail.com>
2024-02-21 16:56:30 -0600

    configuraciones agregadas

---
+++ ServiciosValidador/Services/ICommonService.cs
@@ -0,0 +1,20 @@
+﻿namespace ServiciosValidador.Services
+{
+    public interface ICommonService<T,TI,TU>
+    {
+        public List<string> Errors { get; }
+        Task<IEnumerable<T>> Get();
+
+        Task<T> GetById(int id);
+
+        Task<T> Add(TI statusValidadorIdDTO);
+
+        Task<T> Update(int id, TU statusValidadorDTO);
+
+        Task<T> Delete(int id);
+
+        bool Validate(TI dto);
+
+        bool Validate(TU dto);
+    }
+}
